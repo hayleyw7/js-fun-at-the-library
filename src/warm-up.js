1150bf347c141169ce4aa106067820dade206b2c
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-24 15:39:30 -0600

    Initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,51 +13,119 @@
 // var title = "Harry Potter";
 // var ...
 
-
+var title = "Harry Potter";
+var pages = 4;
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
+// of what it currently is
 
+pagesCondensed = pages / 420
+pages = pagesCondensed
 
+pages
 
 // Initialize a new variable "movie", but leave it undefined
 
+var movie = undefined
 
+movie
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
+var sequel = title + " 2"
 
+sequel
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
+var isFavorite = false
 
-
+isFavorite
 
 // Create a new variable named "isShortStory", and use a comparison operator to
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+// HELP!!!!!!!!!!!!
+// HELP!!!!!!!!!!!!
+// HELP!!!!!!!!!!!!
 
+// function shortStoryDetermination() {
+// 	if (pages < 25) {
+// 		true
+// 	} else {
+// 	false
+// 	}
+// }
+
+// var isShortStory = shortStoryDetermination()
+
+// shortStoryDetermination()
+
+///
+
+// var isShortStory =
+// 	if (pages < 25) {
+//  		true
+//  	} else {
+//  	false
+//  	}
+
+// shortpages
+
+///
+
+// var isShortStory = undefined;
+
+// function shortStoryDetermination() {
+// 	if (pages < 25) {
+// 		var isShortStory = true;
+// 	} else {
+// 		var isShortStory = false;
+// 	}
+// }
+
+// isShortStory
+
+// HELP!!!!!!!!!!!!
+// HELP!!!!!!!!!!!!
+// HELP!!!!!!!!!!!!
 
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
 
+function readTime() {
+		if (pages < 1000) {
+		console.log("I could read that in a day!");
+	} else {
+		console.log("Ok, maybe I could read that in a day and a half")
+	}
+}
 
+readTime()
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
 
+function logIfFavorite() {
+		if (isFavorite === true) {
+		console.log("My favorite book is " + title);
+	} else {
+		console.log("I recently read the " + title + " book")
+	}
+}
 
+logIfFavorite()
 
 //------------------------- Arrays -------------------------//
 
@@ -65,17 +133,30 @@
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
 
+var books = ["Homosapien", "The Stranger", "Breakfast of Champions", "1984", "Animal Farm"]
+
+books
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+// Write the code to access the 3rd element of the Array, using bracket
+// notation.
+
+console.log(books[2])
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
 
+var belowTwoHundred = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+belowTwoHundred
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+// console.log(belowTwoHundred[4])
+
+console.log(belowTwoHundred[4])
 
 //------------------------- Objects -------------------------//
 
@@ -83,6 +164,16 @@
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
 
+var dictionary = {
+	name: "Webster's",
+	hasThesaurus: false,
+}
+
+dictionary
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+dictionary.pageCount = 720
+
+dictionary.pageCount
