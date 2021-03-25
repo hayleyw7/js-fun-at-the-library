680a6e4671102a6b2eebbee53b3666684b417fde
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-24 21:05:34 -0600

    Complete BMC 1

--- src/book.js
+++ src/book.js
@@ -1,17 +1,33 @@
 function createTitle(title) {
   var bookIdea = "Storm's Awakening";
-  return `The ${title}`
+  return `The ${title}`;
 };
 
 var sushiTitle = "Dancing Sushi";
 var dragonTitle = "Dragon in the Summer";
 var ghostTitle = "Teenage Ghoul";
 
+function buildMainCharacter(name, age, pronouns) {
+  name = name;
+  age = age;
+  pronouns = pronouns;
+}
 
+var ghoulCharacter = {
+  name: "Vassya",
+  age: 16,
+  pronouns: "she/her",
+}
+
+var bakerCharacter = {
+  name: "Hannah",
+  age: 30,
+  pronouns: "she/her",
+}
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
