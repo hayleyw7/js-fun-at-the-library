ab762c8eff6c3ae6689bee0344f0c685e1798e3d
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-24 21:21:54 -0600

    Complete BMC

--- src/book.js
+++ src/book.js
@@ -8,21 +8,12 @@ var dragonTitle = "Dragon in the Summer";
 var ghostTitle = "Teenage Ghoul";
 
 function buildMainCharacter(name, age, pronouns) {
-  name = name;
-  age = age;
-  pronouns = pronouns;
-}
-
-var ghoulCharacter = {
-  name: "Vassya",
-  age: 16,
-  pronouns: "she/her",
-}
-
-var bakerCharacter = {
-  name: "Hannah",
-  age: 30,
-  pronouns: "she/her",
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return character
 }
 
 module.exports = {
