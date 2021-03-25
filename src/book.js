0f68a2e311ec1b55643e488f47224003e483b460
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-24 20:26:35 -0600

    Complete createTitle

--- src/book.js
+++ src/book.js
@@ -1,6 +1,13 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  var bookIdea = "Storm's Awakening";
+  return `The ${title}`
+};
+
+var sushiTitle = "Dancing Sushi";
+var dragonTitle = "Dragon in the Summer";
+var ghostTitle = "Teenage Ghoul";
+
+
 
 module.exports = {
   createTitle,
