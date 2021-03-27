18e14184d9a3eb995f778ef19158fa4e3c90595f
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-27 12:41:53 -0600

    Start createLibrary

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,21 @@
+function createLibrary(name, shelves) {
+  name = name;
+  shelves = {
+    fantasy: fantasy,
+    fiction: fiction,
+    nonFiction: nonFiction,
+  }
+  console.log(name, shelves)
+}
 
+
+
+
+
+// npm test test/library-test.js
 
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
