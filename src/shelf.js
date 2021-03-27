871b86817b76d044dd0ffbe7e4f768f458792a75
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-27 14:19:04 -0600

    Remove tests and add notes

--- src/shelf.js
+++ src/shelf.js
@@ -1,47 +1,125 @@
-function shelfBook(book, SciFiShelf) {
-  var book = {
-    title: title,
-    mainCharacter: mainCharacter,
-    pageCount: pageCount,
-    genre: genre,
-  }
-  if (book[3] === "sciFi") {
-    sciFiShelf.push
+// function shelfBook(book, SciFiShelf) {
+//   if (book[3] === "sciFi") {
+//     sciFiShelf.push
+//     //// pop? shift? unshift?
+//   }
+// }
+
+function shelfBook(book, shelf) {
+  shelf.pop(book)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////
+
+/////UNSHELF BOOK!!!!!!!!
+
+//////////
+
+///what i have so far (NOT WORKING)
+// function unshelfBook(book, shelf) {
+//   shelf.unshift(book)
+
+///// add a loop????
+/////  maybe if else inside of a loop???
+//// splice instead of unshift? slice???
+
+// }
+
+
+///// from slack (another person)
+
+// function unshelfBook(book, shelf) {
+//   for (var i = 0; i < shelf.length; i++) {
+//     if (book.title === shelf[i].title) {
+//       var removedBook = shelf.splice(i, 1);
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////doesn't work
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    console.log(shelf[i].title);
+    // if (shelf.includes(title);
+    //   return shelf
+
+    /////add if else
+    //// and add parameters in that expression
   }
 }
+
+////ideas from helper but not working anyway
+
+// for (var i = 0; i < shelf.length; i++) {
+//   if(book.title === shelf[i].title) {
+//     var removedBook = shelf.splice(i, 1);
+//   }
+// }
+
+
+/////idea from slack
+
+// function unshelfBook(book, shelf) {
+//   for (var i = 0; i < shelf.length; i++) {
+//     if (book.title === shelf[i].title) {
+//       var removedBook = shelf.splice(i, 1);
+//     }
+//   }
+
+
+
+///////////
+
+//////list titles!!!!!!
+
+/////////
+
+function listTitles(shelf) {
+  console.log(listTitles)
+}
+
+function searchShelf() {
+}
+
+
 
 
 
 // npm test test/shelf-test.js
 
-///////
-//   var hyperion = {
-//     title: title,
-//     mainCharacter: mainCharacter {
-//       name: name,
-//       age: age,
-//       pronouns: pronouns,
-//     pageCount: pageCount,
-//     genre: genre,
-//   }
-//   var dune = {
-//     title: title,
-//     mainCharacter: mainCharacter {
-//       name: name,
-//       age: age,
-//       pronouns: pronouns,
-//     pageCount: pageCount,
-//     genre: genre,
-//   }
-// }
-
-
-
-
-
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
